1759c50f2aeb673af8a56307a79f80b7a825c8c5
Marios Tsekitsidis <tsekitsi@grinnell.edu>
2019-02-21 02:50:21 -0600

    First commit for assignment 1

---
+++ assignment1/custom.js
@@ -0,0 +1,21 @@
+/*
+// set date input 'picker' to moment 'mom':
+function setPicker2Date(picker, mom) {
+  picker.value = mom.format('YYYY-MM-DD');
+}
+
+// returns a moment object with the designated amount of time added:
+function addDuration2Moment(dur, mom) {
+  var result = mom.add(dur);
+  return result;
+}
+
+var dur = moment.duration(2, 'days');
+var now = moment();
+
+console.log(addDuration2Moment(dur, now).toDate().toString())
+
+function capD() {
+  console.log(moment(document.getElementById("datePicker").value).toDate().toString())
+}
+*/
