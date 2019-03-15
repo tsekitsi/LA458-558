ba25daca5a479c0287f43cf2b0dd82cc374cbb75
Marios Tsekitsidis <tsekitsi@grinnell.edu>
2019-03-14 21:20:08 -0500

    Update script.js

--- ex8/script.js
+++ ex8/script.js
@@ -21,10 +21,27 @@ var baseMaps = {
     "Aerial": esriWorldImagery
 };
 
-var overlayMaps = {
-    "Cities": cities,
-    "Line": myLine,
-    "Polygon": myPolygon
-};
+L.control.layers(baseMaps).addTo(map);
 
-L.control.layers(baseMaps, overlayMaps).addTo(map);
+var myURL = "https://tsekitsi.github.io/LA458-558/data/ex8/last16.geojson";
+
+var geojsonLayer = new L.GeoJSON.AJAX(myURL , {
+	 pointToLayer: function (feature, latlng) {
+        return new L.CircleMarker(latlng, {
+            stroke: true,
+            weight: 2, //stroke weight
+            color: '#000000', //stroke color
+            opacity: 1.0, //stoke opacity
+			fillColor: 'red',
+            fillOpacity: 0.85,
+            radius: 10,
+           //title: this is not supported here - see layer.bindTooltip  below
+        });
+    },
+
+    onEachFeature: function (feature, layer) {
+        htmlText = "<strong>" + feature.properties.name + "</strong><br><img src='http://129.186.142.145/teachingGIS/webgis/cyStatues/" + feature.properties.photos + ".jpg' alt='Smiley face' height='266' width='200'>";
+        layer.bindPopup(htmlText);
+		layer.bindTooltip(feature.properties.name);
+    }
+}).addTo(map);
